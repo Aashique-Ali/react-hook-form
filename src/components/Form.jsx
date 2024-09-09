79563b1d6fca147ca85baf592b8886565a7c18ad
Aashique-Ali <aashique.ali2002@gmail.com>
2024-09-09 12:27:08 +0500

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,66 @@
+import React from "react"
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+const Form = () => {
+  const schema = yup.object().shape({
+    fullName: yup.string().required(),
+    email: yup.string().email().required(),
+    age: yup.number().positive().integer().min(18).required(),
+    password: yup.string().min(8).max(20).required(),
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <div className="bg-green-600 w-full flex justify-center items-center h-[100vh]">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-green-400 flex justify-center items-center gap-3 flex-col p-16 rounded"
+      >
+        <input
+          type="text"
+          placeholder="your fullname"
+          {...register("fullName")}
+          className="w-[15rem] h-8 rounded p-2"
+        />
+        <p className="text-red-600">{errors.fullName?.message}</p>
+        <input
+          type="text"
+          placeholder="your email"
+          {...register("email")}
+          className="w-[15rem] h-8 rounded p-2"
+        />
+        <p className="text-red-600">{errors.email?.message}</p>
+        <input
+          type="number"
+          placeholder="your age"
+          {...register("age")}
+          className="w-[15rem] h-8 rounded p-2"
+        />
+        <p className="text-red-600">{errors.age?.message}</p>
+        <input
+          type="password"
+          placeholder="password"
+          {...register("password")}
+          className="w-[15rem] h-8 rounded p-2"
+        />
+        <p className="text-red-600">{errors.password?.message}</p>
+        <input type="submit" className="" />
+      </form>
+    </div>
+  )
+}
+
+export default Form
