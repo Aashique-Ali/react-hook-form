12aa7b14a622a65a8dd58d97799acfb994f1e38e
Aashique-Ali <aashique.ali2002@gmail.com>
2024-09-09 14:47:42 +0500

    second commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -29,35 +29,38 @@ const Form = () => {
         onSubmit={handleSubmit(onSubmit)}
         className="bg-green-400 flex justify-center items-center gap-3 flex-col p-16 rounded"
       >
+        <h1>Register</h1>
         <input
           type="text"
           placeholder="your fullname"
           {...register("fullName")}
           className="w-[15rem] h-8 rounded p-2"
         />
-        <p className="text-red-600">{errors.fullName?.message}</p>
+        <p className="text-red-600 text-[12px]">{errors.fullName?.message}</p>
         <input
           type="text"
           placeholder="your email"
           {...register("email")}
           className="w-[15rem] h-8 rounded p-2"
         />
-        <p className="text-red-600">{errors.email?.message}</p>
+        <p className="text-red-600 text-[12px]">{errors.email?.message}</p>
         <input
           type="number"
           placeholder="your age"
           {...register("age")}
           className="w-[15rem] h-8 rounded p-2"
         />
-        <p className="text-red-600">{errors.age?.message}</p>
+        <p className="text-red-600 text-[12px]">{errors.age?.message}</p>
         <input
           type="password"
           placeholder="password"
           {...register("password")}
           className="w-[15rem] h-8 rounded p-2"
         />
-        <p className="text-red-600">{errors.password?.message}</p>
-        <input type="submit" className="" />
+        <p className="text-red-600 text-[12px]">{errors.password?.message}</p>
+        <button type="submit" className="py-2 px-4 bg-green-600">
+          Register
+        </button>
       </form>
     </div>
   )
